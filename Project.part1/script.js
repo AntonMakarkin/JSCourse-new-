9916669fdf465a8ac7360cc9b0d539a30c31bf81
Anton Makarkin <amakarkin27@gmail.com>
2020-06-26 23:53:38 +0300

    Practice Part2. logical expressions and cycles

--- Project.part1/script.js
+++ Project.part1/script.js
@@ -1,21 +1,41 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    personalMovieDB = {
-        count: 0,
+    const personalMovieDB = {
+        count: numberOfFilms,
         movies: {},
         actors: {},
         genres: [],
         privat: false
-    },
+    };
 
-    firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
-    secondQuestion = prompt('На сколько оцените его?', ''),
-    thirdQuestion = prompt('Один из последних просмотренных фильмов?', ''),
-    fourthQuestion = prompt('На сколько оцените его?', '');
+    for (let i = 0; i < 2; i++) {
+        const firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
+              secondQuestion = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[firstQuestion] = secondQuestion;
-personalMovieDB.movies[thirdQuestion] = fourthQuestion;
+        if(firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length < 50) {
+            personalMovieDB.movies[firstQuestion] = secondQuestion;
+            console.log('done');
+        }
+        else {
+            console.log('error');
+            i--;
+        }
+        
+    }
+
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотренно довольно мало фильмов");
+    }
+    else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    }
+    else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    }
+    else {
+        console.log("Произошла ошибка");
+    }
 
 console.log(personalMovieDB);
